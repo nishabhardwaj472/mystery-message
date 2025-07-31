5470b7215846a7a9cb48f784ed754a38185459e6
nishabhardwaj472 <nishabhardwaj843328@gmail.com>
2025-07-31 20:57:00 +0530

    schemas

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,10 @@
+import {z} from 'zod'
+
+export const messageSchema = z.object({
+    content: z
+    .string()
+    .min(10,{message: 'Content must be atleast of 10 characters'})
+    .max(300,{message: 'Content must be no longer tham 300 characters'})
+
+    
+})
